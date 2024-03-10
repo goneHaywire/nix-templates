369eaa38116dd62304e3e969c209da97911c0231
Emiljan Dusha <emiljanthegreat@gmail.com>
2024-03-10 18:16:29 +0100

    added more template sources

--- flake.nix
+++ flake.nix
@@ -1,13 +1,19 @@
 {
   description = "My personal flake templates repo";
-  # TODO: add other template repos
 
-  outputs = { self }: {
+  inputs = {
+    dev-templates.url = "github:the-nix-way/dev-templates";
+    nixos-templates.url = "github:nixos/templates";
+  };
+
+  outputs = { self, dev-templates, nixos-templates }: {
     templates = {
       my-rust = {
         path = ./my-rust;
         description = "Personal rust flake template";
       };
-    };
+    }
+    // dev-templates.templates
+    // nixos-templates.templates;
   };
 }
